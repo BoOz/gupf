41fc1c73781da1be60ff257d944836c7c1a9696b
BoOz <booz@rezo.net>
2017-01-19 22:51:07 +0100

    install

--- config/install.sh
+++ config/install.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
 # Dans le terminal taper :
-# cd chemin/gupf/config
-# chmod +x docteur.sh
+# cd gupf/config
+# chmod +x install.sh
 # puis glisser install.sh dans le terminal et valider.
 
-echo "Vérification de la configuration de la gestion des utilisateurs par fichiers"
+echo "\nVérification de la configuration des scripts de gestion des utilisateurs par fichiers.\n\n"
+
+echo "Installation OK.\n"
+
+
