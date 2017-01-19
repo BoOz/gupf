723945561fc0eba1b72a4ec6d9cd140599170281
BoOz <booz@rezo.net>
2017-01-19 22:14:45 +0100

    Script de configuration
    Dans le terminal taper :
    `cd chemin/gupf/config`
    `chmod +x docteur.sh`
    
    Puis glisser install.sh dans le terminal et valider.

---
+++ config/install.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# Dans le terminal taper :
+# cd chemin/gupf/config
+# chmod +x docteur.sh
+# puis glisser install.sh dans le terminal et valider.
+
+echo "Vérification de la configuration de la gestion des utilisateurs par fichiers"
