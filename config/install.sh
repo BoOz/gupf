e8d717ce9e76f8baeeaf4956bae712ec8abd7668
BoOz <booz@rezo.net>
2017-01-20 00:27:31 +0100

    gestion de l'installation des commandes

--- config/install.sh
+++ config/install.sh
@@ -11,8 +11,33 @@ echo "***************************************"
 echo "Gestion des utilisateurs par fichiers."
 echo "***************************************"
 
-echo "\nVérification de la configuration des scripts : "
-echo "- Installation OK."
+echo "\nVérification des commandes.\n"
+
+# Ou sommes nous ?
+repertoire=${0%/*}
+repertoire=${repertoire/\/config/}
+
+echo "Commandes disponibles : "
+for c in $(find $repertoire/commandes/*); do
+	commande=${c##*/}
+	commande=${commande/.sh/}
+	echo "- $commande"
+
+	# La commande est-elle installée ?
+	echo "Ajouter la commande $commande à votre profil dans le terminal en copiant"
+	echo "alias dedup=\"$repertoire/commandes/dedup.sh\""
+	echo "dans le fichier $HOME/.bash_profile. \nPasser en mode édition en tapant i puis esc :wq pour sauvegarder"
+	echo "vim $HOME/.bash_profile\n"
+
+	echo "Relancer le terminal"
+	echo ". $HOME/.bash_profile\n"
+
+	echo "Usage : "
+	echo "$commande fichier1.txt fichier2.txt"
+
+done
+
+echo "\n- Installation OK."
 echo "\n\n\n\n"
 
 
