a9ef8559d3a740a20703ed4fceb443b096351c65
BoOz <booz@rezo.net>
2017-01-30 18:35:31 +0100

    simplifier

--- commandes/wav2mp3.sh
+++ commandes/wav2mp3.sh
@@ -12,22 +12,17 @@ if [[ "$source" == "" ]] ; then
 	source="$PWD"
 fi
 
-echo "\n* Conversion mp3 48 000 Hz des wav situés dans $source\n"
-
-num=${source##*/}
-rep=${source%/*}
+echo "\n* Conversion mp3 48 000 Hz des wav : $source\n"
 
 find "$source" -name "*.wav" | while read f; do
 
 	# Si on a affaire à un fichier wav, on le converti à coté de la ou il est.
-	if [ ! -d "$source" ] ; then
+	if [ -f "$source" ] ; then
 		ffmpeg -i "$f" -codec:a libmp3lame -qscale:a 2 -ar 48000 -ab 192k "${source/wav/mp3}" < /dev/null;
 	else
-	# si on a affaire à un répertoire
-		[[ "$num" == "wav" ]] && dest="${rep}/mp3" # si le répertoire s'appelle wav on crée à coté un répertoire mp3
-		[[ "$num" == "wav" ]] || dest="${source}" # sinon on converti sur place.
-
-		[ ! -d "$dest" ] && mkdir "$dest"
+	# si on a affaire à un répertoire wav dans le path, on crée un repertoire mp3
+		dest=${source/wav/mp3}
+		[ ! -d "$dest" ] && mkdir -p "$dest"
 	
 		file=$(basename "$f")
 		file="${file// /_}"
