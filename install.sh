ac5eda3db84ccd2287d864f4a1f22e1ccf7a4250
BoOz <booz@rezo.net>
2017-04-05 12:10:52 +0200

    maj install

--- install.sh
+++ install.sh
@@ -13,28 +13,30 @@ echo "***************************************"
 echo "Commandes en plus dans le terminal"
 echo "***************************************"
 
-echo "\nVérification des commandes.\n"
+echo "Installation et mise à jour"
+echo "Ajouter les commandes \`terminal\` dans le fichier \`$HOME/.bash_profile\`."
+echo "vim $HOME/.bash_profile # dans une nouvelle fenêtre du terminal.\n"
+echo "Taper \`i\` et copier les lignes ci-dessous puis taper \`esc :wq\` pour sauvegarder."
 
-echo "Commandes disponibles : "
+echo "Relancer le terminal"
+echo ". $HOME/.bash_profile\n"
+
+echo "\nCommandes à copier-coller dans le fichier \`.bash_profile\` \n"
+
+echo "# Config terminal ++\n"
+echo "# terminal"
+echo "alias terminal=\"cd /Users/vincent/Sites/github/terminal ; git pull ; ./install.sh\""
+echo "# Usage : terminal\n"
 find $repertoire/commandes/* | while read c
 do
 	f=$(realpath $c)
 	commande=${c##*/}
 	commande=${commande/.sh/}
-	echo "- $commande"
+	echo "# $commande"
 
 	# La commande est-elle installée ?
-	echo "Ajouter la commande \`$commande\` dans le fichier \`$HOME/.bash_profile\`."
-	echo "vim $HOME/.bash_profile # dans une nouvelle fenêtre du terminal.\n"
-	echo "Taper \`i\` et copier la ligne ci-dessous :"
 	echo "alias $commande=\"$f\""
-	echo "Taper \`esc :wq\` pour sauvegarder."
-	
-	echo "Relancer le terminal"
-	echo ". $HOME/.bash_profile\n"
-	# . $HOME/.bash_profile
-
-	echo "Usage : $commande help\n\n"
+	echo "# Usage : $commande help\n"
 
 done
 
